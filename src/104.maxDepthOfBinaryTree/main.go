fe507b6988e229c0957c55dfd15e757b963cb9b0
Nemo <ssdn@vip.qq.com>
2018-08-30 13:14:47 +0800

    104 max depth of binary tree

---
+++ src/104.maxDepthOfBinaryTree/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	tree := &TreeNode{1, nil, &TreeNode{2, nil, nil}}
+	r := maxDepth(tree)
+
+	fmt.Println("max depth:", r)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//func maxDepth(root *TreeNode) int {
+//	if root == nil {
+//		return 0
+//	}
+//	return maxDepth2(root, 0)
+//}
+//
+//func maxDepth2(node *TreeNode, depth int) int {
+//	if node == nil {
+//		return depth
+//	}
+//	return int(math.Max(float64(maxDepth2(node.Left, depth+1)), float64(maxDepth2(node.Right, depth+1))))
+//}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
