c61b2c6d17dd49aa3b43332f4540cc7588b85032
Nemo <ssdn@vip.qq.com>
2018-09-04 13:46:19 +0800

    110 balanced binary tree O(n)

--- src/110.balancedBinaryTree/main.go
+++ src/110.balancedBinaryTree/main.go
@@ -18,24 +18,48 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// O(n)
+//func isBalanced(root *TreeNode) bool {
+//	if root == nil {
+//		return true
+//	}
+//
+//	if root.Left == nil && root.Right == nil {
+//		return true
+//	}
+//
+//	return math.Abs(float64(height(root.Left)-height(root.Right))) <= 1 && isBalanced(root.Left) && isBalanced(root.Right)
+//}
+//
+//func height(root *TreeNode) int {
+//	if root == nil {
+//		return 0
+//	}
+//	if root.Left == nil && root.Right == nil {
+//		return 1
+//	}
+//	return 1 + int(math.Max(float64((height(root.Left))), float64(height(root.Right))))
+//}
+
+// dfs 方式O(n)
 func isBalanced(root *TreeNode) bool {
-	if root == nil {
-		return true
-	}
-
-	if root.Left == nil && root.Right == nil {
-		return true
-	}
-
-	return math.Abs(float64(height(root.Left)-height(root.Right))) <= 1 && isBalanced(root.Left) && isBalanced(root.Right)
+	return height(root) != -1
 }
 
-func height(root *TreeNode) int {
-	if root == nil {
+func height(node *TreeNode) int {
+	if node == nil {
 		return 0
 	}
-	if root.Left == nil && root.Right == nil {
-		return 1
+	left := height(node.Left)
+	if left == -1 {
+		return -1
 	}
-	return 1 + int(math.Max(float64((height(root.Left))), float64(height(root.Right))))
+	right := height(node.Right)
+	if right == -1 {
+		return -1
+	}
+	if math.Abs(float64(left)-float64(right)) > 1 {
+		return -1
+	}
+	return 1 + int(math.Max(float64(left), float64(right)))
 }
